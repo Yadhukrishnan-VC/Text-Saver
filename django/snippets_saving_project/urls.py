506c189f2d4dc04c59ee44939d8548ef0f350b4c
YADHU <yadhukrishnanvc7@gmail.com>
2023-05-20 11:57:29 +0530

    Create Authentication APIs & CRUD APIs

--- django/snippets_saving_project/urls.py
+++ django/snippets_saving_project/urls.py
@@ -20,4 +20,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',TemplateView.as_view(template_name='index.html')),
+    path('api/', include("snippets_app.urls")),
 ]
